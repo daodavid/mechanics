1f27c78cbbf13bafb93dde75871552702851efdf
daodeiv <daodeiv@Abv.bg>
2019-05-19 20:52:00 +0300

    innit project added ploting for field

--- daomath/daomechanics.py
+++ daomath/daomechanics.py
@@ -5,7 +5,7 @@ from daomath.numdao import *
 
 class VectorField:
 
-    def __init__(self, x_function, y_function, z_function=None, range=[1, 10]):
+    def __init__(self, x_function, y_function, z_function=None, range=[-10, 10],coridinates=None):
         """
 
         :param x_function: e1 = U(x,y)
@@ -13,14 +13,18 @@ class VectorField:
         :param z_function: e3 = P(x,y)
         :param range:
         """
-        self.U = x_function
-        self.V = y_function
-        self.z = z_function
-        self.rng = range
-        self.evaluate_cord_field()
+        if coridinates is not None:
+            self.field_cordinates=coridinates
+        else :
+           self.U = x_function
+           self.V = y_function
+           self.z = z_function
+           self.rng = range
+           self.evaluate_cord_field()
 
-    def plot_field(self, matrix, color='b', reduce=20, scale=5, width=0.003):
-        v = matrix
+
+    def plot_field(self, color='b', reduce=20, scale=5, width=0.003,label=r'$\vec F$'):
+        v = self.field_cordinates
         x0 = v[:, 0]
         y0 = v[:, 1]
         x = v[:, 2]
@@ -30,12 +34,23 @@ class VectorField:
         x = reduce_array(x, reduce)
         y = reduce_array(y, reduce)
 
-        q = plt.quiver(x0, y0, x, y, angles='xy', scale_units='xy', scale=scale, color=color, width=width)
+        q = plt.quiver(x0, y0, x, y, angles='xy', scale_units='xy', scale=scale, color=color, width=width,label=label,linewidth=1)
+        #line3, = plt.plot([0, 0, 0], label=r'$\vec F$', linewidth=1, color=color)
+        ax = plt.gca()
+        #ax.plot([10, 6, 10])
+        plt.legend(loc='upper left')
+        ax.spines['top'].set_color('none')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['left'].set_position('zero')
+        ax.spines['right'].set_color('none')
+        plt.text(self.rng[1], self.rng[1], label, fontsize=20, verticalalignment='center')
+        plt.xlim(self.rng[0], self.rng[1])
+        plt.ylim(self.rng[0], self.rng[1])
 
     def evaluate_cord_field(self):
-        x = np.linspace(self.rng[0], self.rng[1], 80)
-        y = np.linspace(self.rng[0], self.rng[1], 80)
-        self.field_cordinates = np.array([[x0, y0, self.U(0, x0, y0), self.V(0, x0, y0)] for y0 in y for x0 in x])
+        x = np.linspace(self.rng[0], self.rng[1], 50)
+        y = np.linspace(self.rng[0], self.rng[1], 50)
+        self.field_cordinates = np.array([[x0, y0, self.U(x0, y0), self.V(x0, y0)] for y0 in y for x0 in x])
 
     def get_coridinates(self):
         return self.field_cordinates
@@ -57,3 +72,19 @@ class VectorField:
 
     def get_field_cordinates(self):
         return self.field_cordinates
+
+
+    def sum(vector_field1 ,vecor_field2):
+        v1 = vector_field1
+        v2 = vecor_field2
+
+        x0 = v1[:,0]
+        y0 = v1[:,0]
+        x =  v1[:,1]+v2[:,1]
+        y = v1[:, 2] + v2[:, 2]
+        return VectorField(np.array([x0,y0,x,y]).T)
+
+
+f = VectorField(lambda x,y : 0 , lambda x,y : -9.8)
+f.plot_field(reduce=8,scale=10,width=0.003,label=r'$-g.\vec{j}$')
+plt.show()
