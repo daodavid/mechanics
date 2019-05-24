6044722294fce9b32d388f6232f51b4980d040d2
daodeiv <daodeiv@Abv.bg>
2019-05-24 12:34:41 +0300

    The functionality which calculate the cordinates if point per time evualtion in ground *** must to be implement abstract method calculate_momentum_conversation()

--- daomath/daomechanics.py
+++ daomath/daomechanics.py
@@ -52,7 +52,7 @@ class VectorField:
         x = np.linspace(self.rng[0], self.rng[1], 50)
         y = np.linspace(self.rng[0], self.rng[1], 50)
         t = np.linspace(self.rng[0], self.rng[1], 50)
-        self.field_cordinates = np.array([[x0, y0, self.U(t[0],x0, y0), self.V(t[0],x0, y0)] for y0 in y for x0 in x])
+        self.field_cordinates = np.array([[x0, y0, self.U(t[0], x0, y0), self.V(t[0], x0, y0)] for y0 in y for x0 in x])
 
     def get_coridinates(self):
         return self.field_cordinates
@@ -99,16 +99,24 @@ class Force(VectorField):
 
 class MaterialPoint():
 
-    def __init__(self, x0=0, y0=0, mass=1):
+    def __init__(self, x0=0, y0=0, mass=1, v_x0=0, v_y0=0):
         self.x0 = x0
         self.y0 = y0
+        self.v_x0 = v_x0
+        self.v_y0 = v_y0
         self.mass = mass
+        self.r = np.array([[x0], [y0], [v_x0], [v_y0], [0]])
+        self.last_x = x0
+        self.last_y = y0
+        self.last_vx=v_x0
+        self.last_vy=v_y0
+        self.x_args = [x0]
+        self.y_args = [y0]
+
+    def __get_coordinate__(self, time):
         pass
 
-    def __get_cordinate__(self, time):
-        pass
-
-    def __get_cordinate__(self):
+    def __get_coordinate__(self):
         pass
 
     def get_speed(self):
@@ -120,7 +128,11 @@ class MaterialPoint():
     def get_mass(self):
         return self.mass
 
-    def get_radius_vecotor(self):
+    def set_last_coor(self, x, y):
+        self.last_x = x
+        self.last_y = y
+
+    def get_radius_vector(self):
         pass
 
     def add_force_field(self, f):
@@ -128,6 +140,9 @@ class MaterialPoint():
 
     def add_force(self, force):
         self.force = force
+
+    def get_init_speed(self):
+        return np.array([self.v_x0, self.v_y0])
 
     def calculate_speed(self, vx0, vy0, time_range=[0, 100], step=60):
         self.vx0 = vx0
@@ -137,16 +152,36 @@ class MaterialPoint():
     def draw_speed(self):
         self.force.plot_field(self.speed_space, color='green')
 
-    def calculate_radius_vector(self, vx0, vy0,n=10,h=0.01):
+    def calculate_radius_vector(self, vx0, vy0, n=10, h=0.01):
         # self.r = leapFrog(self.force.U,self.force.V,vx0,vy0,h=0.0001)
         # self.x_args = intergrate(self.x0,self.speed_space[:,0],self.speed_space[:,2])
         # self.y_args = intergrate(self.y0, self.speed_space[:,1], self.speed_space[:,2])
-        #k = 7
+        # k = 7
         # fx = lambda t, x, y: -k * x / ((x ** 2 + y ** 2) ** (3 / 2))
         # fy = lambda t, x, y: -k * y / ((x ** 2 + y ** 2) ** (3 / 2))
-        self.r = leapFrog(self.force.U, self.force.V, self.x0, self.y0, vx0, vy0,n=n, h=h, c=self.mass)
-        print(self.r.shape[0])
+        self.r = leapFrog(self.force.U, self.force.V, self.x0, self.y0, vx0, vy0, n=n, h=h, c=self.mass)
+        last = self.r.shape[0]
+        self.last_x = self.r[last:0]
+        self.last_y = self.r[last:1]
+        self.x_args = self.r[:, 0]
+        self.x_args = self.r[:, 1]
         return self.r
+
+    def set_radius_vector(self, arr):
+        pass
+
+    def append_coordinates(self,x,y):
+        self.last_x=x
+        self.last_x=y
+        self.x_args.append(x)
+        self.y_args.append(y)
+
+
+    def get_radius_vector(self):
+        return self.r
+
+    def get_coordinates(self):
+        return np.array([self.x_args,self.y_args]).T
 
     def plot_radios_vector(self):
         n = 10
@@ -187,10 +222,7 @@ class MaterialPoint():
         size = self.r.shape[0] / z
         return size
 
-    def update_HTML_animation(self, i,arg):
-
-
-
+    def update_HTML_animation(self, i, arg):
         ax = plt.gca()
 
         # q =ax.quiver(0, 0, self.r[i,2],  self.r[i,3], pivot='mid', color='r', units='inches')
@@ -208,13 +240,10 @@ class MaterialPoint():
         particles.set_data(self.r[i, 0], self.r[i, 1])
         particles.set_markersize(20)
 
-
-
-        q = plt.scatter(self.r[i, 0], self.r[i, 1], color='black',linewidths=5)
+        q = plt.scatter(self.r[i, 0], self.r[i, 1], color='black', linewidths=5)
         q = plt.scatter(0, 0, color='black')
 
         rezult = particles
-
 
         z = plt.plot(self.r[:, 0], self.r[:, 1], color='blue')
         plt.draw()
@@ -222,24 +251,20 @@ class MaterialPoint():
         return particles
         #
 
-
-
-
-
 # z = point.calculate_radius_vector(3,4)
 # anim = animation.FuncAnimation(fig, point.update_HTML_animation, fargs=(Q, X, Y),
 #
 #                                interval=50, blit=False)
 
-k = 10
-u = lambda t, x, y: -400 * x / ((x ** 2 + y ** 2) ** (3 / 2))
-v = lambda t, x, y: -400 * y / ((x ** 2 + y ** 2) ** (3 / 2))
-u = lambda t, x, y: 0
-v = lambda t, x, y: -10
-point = MaterialPoint(x0=0, y0=0, mass=1)
-f = VectorField(u, v)
-point.add_force(f)
-z = point.calculate_radius_vector(20*np.cos(np.pi/4), +50*np.sin(np.pi/4),n=1000)
+# k = 10
+# u = lambda t, x, y: -400 * x / ((x ** 2 + y ** 2) ** (3 / 2))
+# v = lambda t, x, y: -400 * y / ((x ** 2 + y ** 2) ** (3 / 2))
+# u = lambda t, x, y: 0
+# v = lambda t, x, y: -10
+# point = MaterialPoint(x0=0, y0=0, mass=1)
+# f = VectorField(u, v)
+# point.add_force(f)
+# z = point.calculate_radius_vector(20*np.cos(np.pi/4), +50*np.sin(np.pi/4),n=1000)
 
 # point.plot_graph()
 # size = int(point.get_size(1))
--- daomath/ground.py
+++ daomath/ground.py
@@ -3,5 +3,65 @@ import matplotlib.pyplot as plt
 from daomath.numdao import *
 from matplotlib import animation, rc
 from IPython.display import HTML
+import math
+from daomath.daomechanics import MaterialPoint
+from daomath.numdao import *
 
 
+class Ground:
+    def __init__(self):
+        self.points = []
+
+    def add_point(self, point):
+        self.points.append(point)
+
+    def calculate_speed_points(self, end_time=1000, h=0.1):
+        times = []
+        c = 0.5
+
+        for i in range(len(self.points)):
+            is_has_collision = False
+            for j in range(len(self.points)):
+                p = self.points[i]
+                p1 = self.points[j]
+                if i != j and ((abs(p.last_x - p1.last_x)) <= c) and (abs(p.last_y - p1.last_y) <= c):
+                    self.calculate_momentum_conservation(p, p1)
+                    is_has_collision = True
+                    break
+
+            if not is_has_collision:
+                self.__update_coordinates(p)
+
+    def calculate_momentum_conservation(self, p, p1):
+        pass
+
+    def __update_coordinates(self, p, h=0.1):
+        """
+        update cordinate with one incremented(integrated) step
+
+        :param p: Material point
+        :param vx_0: init value for vx cordinate
+        :param vy_0: init value for vy cordinate
+        :param h: increment integration step
+        :return:
+        """
+        x = p.last_x
+        y = p.last_y
+        vx = p.last_vx
+        vy = p.last_vy
+
+        x = x + h * vx
+        y = y + h * vy
+        p.append_coordinates(x, y)
+
+
+g = Ground()
+g.add_point(MaterialPoint(x0=0, y0=0, mass=3, v_x0=1, v_y0=2))
+g.add_point(MaterialPoint(x0=3, y0=3, mass=3, v_x0=3, v_y0=4))
+g.add_point(MaterialPoint(x0=3.1, y0=3.1, mass=3, v_x0=3, v_y0=4))
+g.add_point(MaterialPoint(x0=7, y0=7, mass=3, v_x0=1, v_y0=-2))
+g.calculate_speed_points()
+
+points = g.points
+for p in points:
+    print(p.get_coordinates())
